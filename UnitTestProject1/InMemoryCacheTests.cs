5a1009bdbf1b38877aa4f45f33047170aeec61e0
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-07-19 23:09:12 +0100

    Added InMemoryCacheTests

---
+++ UnitTestProject1/InMemoryCacheTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class InMemoryCacheTests
+    {
+        [TestMethod]
+        public void ExampleTestMethod()
+        {
+            var expectedData = new byte[] { 100, 200 };
+
+            var opts = Options.Create<MemoryDistributedCacheOptions>(new MemoryDistributedCacheOptions());
+            IDistributedCache cache = new MemoryDistributedCache(opts);
+            cache.Set("key1", expectedData);
+            var cachedData = cache.Get("key1");
+
+            Assert.AreEqual(expectedData, cachedData);
+
+            //Use the variable cache as an input to any class which expects IDistributedCache
+        }
+    }
+}
